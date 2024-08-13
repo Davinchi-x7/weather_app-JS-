27f9401aebd0b1b081fa2b4446acb4a1cb4297aa
David Ngugi <124037037+Davinchi-x7@users.noreply.github.com>
2024-08-13 17:54:35 +0300

    hiding api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        const apiKey = "190ccc96e03683da742bcfcb538999c8";
+        const apiKey = "input your API key";
         const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
         
         const searchBox = document.querySelector(".search input");
